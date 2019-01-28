58225477c823bbf3b650e0ba17635b19a71b96f3
Dogac Akyıldız <ndakyildiz@gmail.com>
2019-01-28 14:32:34 +0300

    Update mapping-dataframe.py

--- mapping-dataframe.py
+++ mapping-dataframe.py
@@ -10,3 +10,12 @@ train['Sex_Male'] = train.Sex.map('female': 0, 'male': 1)
 train.head()
 
 # You can see the head of the dataframe, and see the new Sex_Male column at the right
+
+# In another way, you can do this by get_dummies method below
+
+pd.get_dummies(train.Sex, prefix='Sex').iloc[:, 1:]
+
+# It seems like more work, but it is more flexible than mapping
+
+# iloc is used to select all rows and second column
+# train.Sex is a Series, prefix is a name before the sex eg. Sex_male
